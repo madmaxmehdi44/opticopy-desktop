75d8e5449ee10d8d2a9c11be40f222c0d93c11fa
Medi <madmaxmehdi44@gmail.com>
2026-08-26 13:02:15 +0330

    windows: wire main window initialization

---
+++ src/OptiCopy.Windows/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Windowing;
+using WinRT.Interop;
+
+namespace OptiCopy.Windows;
+
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        var handle = WindowNative.GetWindowHandle(this);
+        var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(handle);
+        var appWindow = AppWindow.GetFromWindowId(windowId);
+        if (appWindow is not null)
+            appWindow.Resize(new Windows.Graphics.SizeInt32(1180, 760));
+    }
+}
