69ea61e79e91712e37cb85308b6566562e3b9e91
Medi <madmaxmehdi44@gmail.com>
2026-08-28 12:10:41 +0330

    Add BGRA raster path for WinUI QR display

--- src/OptiCopy.Imaging/Qr/QrMatrixRasterizer.cs
+++ src/OptiCopy.Imaging/Qr/QrMatrixRasterizer.cs
@@ -35,4 +35,45 @@ public static class QrMatrixRasterizer
 
         return pixels;
     }
+
+    public static byte[] ToBgra32(QrMatrix matrix, int scale = 1, int quietZone = 0)
+    {
+        ArgumentNullException.ThrowIfNull(matrix);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(scale);
+        ArgumentOutOfRangeException.ThrowIfNegative(quietZone);
+
+        var moduleWidth = checked(matrix.Width + quietZone * 2);
+        var moduleHeight = checked(matrix.Height + quietZone * 2);
+        var width = checked(moduleWidth * scale);
+        var height = checked(moduleHeight * scale);
+        var pixels = new byte[checked(width * height * 4)];
+        pixels.AsSpan().Fill(0xFF);
+
+        for (var moduleY = 0; moduleY < matrix.Height; moduleY++)
+        {
+            for (var moduleX = 0; moduleX < matrix.Width; moduleX++)
+            {
+                if (!matrix[moduleX, moduleY])
+                    continue;
+
+                var startX = checked((moduleX + quietZone) * scale);
+                var startY = checked((moduleY + quietZone) * scale);
+
+                for (var y = 0; y < scale; y++)
+                {
+                    var row = checked((startY + y) * width + startX);
+                    for (var x = 0; x < scale; x++)
+                    {
+                        var offset = checked((row + x) * 4);
+                        pixels[offset] = 0;
+                        pixels[offset + 1] = 0;
+                        pixels[offset + 2] = 0;
+                        pixels[offset + 3] = 255;
+                    }
+                }
+            }
+        }
+
+        return pixels;
+    }
 }
